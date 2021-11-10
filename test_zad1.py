54482716684de27040575e5b9349ac44c9621fb6
wolnikowa <78367914+wolnikowa@users.noreply.github.com>
2021-11-10 11:11:00 +0100

    initial

---
+++ test_zad1.py
@@ -0,0 +1,57 @@
+import unittest
+
+class hamming():
+    def distance(s1,s2):
+        return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
+
+
+
+class HammingTest(unittest.TestCase):
+    def test_empty_strands(self):
+        self.assertEqual(hamming.distance("", ""), 0)
+
+    # @unittest.skip
+    def test_single_letter_identical_strands(self):
+        self.assertEqual(hamming.distance("A", "A"), 0)
+
+    # @unittest.skip
+    def test_single_letter_different_strands(self):
+        self.assertEqual(hamming.distance("G", "T"), 1)
+
+    # @unittest.skip
+    def test_long_identical_strands(self):
+        self.assertEqual(hamming.distance("GGACTGAAATCTG", "GGACTGAAATCTG"), 0)
+
+    # @unittest.skip
+    def test_long_different_strands(self):
+        self.assertEqual(hamming.distance("GGACGGATTCTG", "AGGACGGATTCT"), 9)
+
+    @unittest.skip
+    def test_disallow_first_strand_longer(self):
+        with self.assertRaisesWithMessage(ValueError):
+            hamming.distance("AATG", "AAA")
+
+    @unittest.skip
+    def test_disallow_second_strand_longer(self):
+        with self.assertRaisesWithMessage(ValueError):
+            hamming.distance("ATA", "AGTG")
+
+    @unittest.skip
+    def test_disallow_left_empty_strand(self):
+        with self.assertRaisesWithMessage(ValueError):
+            hamming.distance("", "G")
+
+    @unittest.skip
+    def test_disallow_right_empty_strand(self):
+        with self.assertRaisesWithMessage(ValueError):
+            hamming.distance("G", "")
+
+    # Utility functions
+    def setUp(self):
+        try:
+            self.assertRaisesRegex
+        except AttributeError:
+            self.assertRaisesRegex = self.assertRaisesRegexp
+
+    def assertRaisesWithMessage(self, exception):
+        return self.assertRaisesRegex(exception, r".+")
