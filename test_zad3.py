e1c724282eae747dc5a72a09385b127beedb7224
wolnikowa <78367914+wolnikowa@users.noreply.github.com>
2021-11-13 22:57:53 +0100

    ci test

--- test_zad3.py
+++ test_zad3.py
@@ -11,6 +11,7 @@ versesList=['On the first day of Christmas my true love gave to me: a Partridge 
             'On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.']
 
 
+
 class PrintSong():
 
     def printSingleVerse(n):
